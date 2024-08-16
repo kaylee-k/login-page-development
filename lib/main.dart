572d1ca0c61f43b619387e96b63e45ff9d97de5b
kaylee-k <kaylee.kihm@gmail.com>
2024-08-15 20:08:37 -0600

    Complete basic UI layout for login screen

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,52 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: false,
+      ),
       home: Scaffold(
-        appBar: AppBar(),
+        appBar: AppBar(
+          title: Text(
+            "Kaylee's iphone",
+            style: TextStyle(fontSize: 28),
+          ),
+          centerTitle: true,
+        ),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(32.0),
+                  child: Image.network(
+                    "https://i.imgur.com/qltk2ip.png",
+                    width: 120,
+                  ),
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                    labelText: 'email',
+                  ),
+                ),
+                TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    labelText: 'password',
+                  ),
+                ),
+                Container(
+                  width: double.infinity,
+                  margin: const EdgeInsets.only(top: 16),
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: Text('Login'),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
